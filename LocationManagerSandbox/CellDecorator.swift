911eae02bbc3e79a23ab10487fe999009b9e905c
Jesse Farless <solidcell@gmail.com>
2016-11-10 13:06:14 +0100

    Colorize booleans and authorization statuses

--- LocationManagerSandbox/CellDecorator.swift
+++ LocationManagerSandbox/CellDecorator.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreLocation
 
 class CellDecorator {
 
@@ -9,26 +10,50 @@ class CellDecorator {
                           subtitle: String(describing: launchOptions))
         case .didChangeAuthorization(let status):
             cell.decorate(title: "locationManager(_:didChangeAuthorization:)",
-                          subtitle: String(describing: status))
+                          subtitle: String(describing: status),
+                          subtitleColor: status.color)
         case .didUpdateLocations(let locations):
             cell.decorate(title: "locationManager(_:didUpdateLocations:)",
                           subtitle: String(describing: locations))
         case .authorizationStatus(let status):
             cell.decorate(title: "authorizationStatus",
-                          subtitle: String(describing: status))
+                          subtitle: String(describing: status),
+                          subtitleColor: status.color)
         case .locationServicesEnabled(let isEnabled):
             cell.decorate(title: "locationServicesEnabled",
-                          subtitle: isEnabled.description)
+                          subtitle: isEnabled.description,
+                          subtitleColor: isEnabled ? UIColor.green : UIColor.red)
         }
     }
 
 }
 
+private extension CLAuthorizationStatus {
+
+    private var good: UIColor { return UIColor.green }
+    private var bad: UIColor { return UIColor.red }
+    private var neutral: UIColor { return UIColor.black }
+
+    var color: UIColor {
+        switch self {
+        case .notDetermined: return neutral
+        case .authorizedAlways: return good
+        case .authorizedWhenInUse: return good
+        case .denied: return bad
+        case .restricted: return bad
+        }
+    }
+    
+}
+
 private extension UITableViewCell {
 
-    func decorate(title: String, subtitle: String? = nil) {
+    func decorate(title: String,
+                  subtitle: String? = nil,
+                  subtitleColor: UIColor? = UIColor.black) {
         textLabel?.text = title
         detailTextLabel?.text = subtitle
+        detailTextLabel?.textColor = subtitleColor
     }
     
 }
